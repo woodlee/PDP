eb924c3441fa85450b68ba33f756995bf7cfecdf
Marty Woodlee <marty.woodlee@gmail.com>
2011-09-05 17:47:59 -0500

    Page 30

--- cms/urls.py
+++ cms/urls.py
@@ -13,5 +13,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^tiny_mce/(?P<path>.*)$', 'django.views.static.serve', 
 		{ 'document_root': '/Users/woodlee/Dev/tinymce/jscripts/tiny_mce' }),
+	url(r'^search/$', 'cms.search.views.search'),
     url(r'', include('django.contrib.flatpages.urls')),
 )
