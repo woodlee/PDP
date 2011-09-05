2fd91d957451dcb03482a3d1c0b1ae3d2d03c3b1
Marty Woodlee <marty.woodlee@gmail.com>
2011-09-05 17:22:56 -0500

    Page 26

--- cms/settings.py
+++ cms/settings.py
@@ -102,6 +102,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    '/Users/woodlee/Dev/PDP/cms/templates',
 )
 
 INSTALLED_APPS = (
@@ -113,6 +114,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.flatpages',
+    'django.contrib.admindocs',
 )
 
 # A sample logging configuration. The only tangible logging
--- cms/urls.py
+++ cms/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
-
 from django.contrib import admin
+
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -8,8 +9,9 @@ urlpatterns = patterns('',
     # url(r'^$', 'cms.views.home', name='home'),
     # url(r'^cms/', include('cms.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^tiny_mce/(?P<path>.*)$', 'django.views.static.serve', 
+		{ 'document_root': '/Users/woodlee/Dev/tinymce/jscripts/tiny_mce' }),
+    url(r'', include('django.contrib.flatpages.urls')),
 )
